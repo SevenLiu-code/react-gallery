e499aac5b2b8135da78a64f41e2937db334f0745
liubin <752791962@qq.com>
2017-03-07 17:47:49 +0800

    2017/3/7

--- script/data.js
+++ script/data.js
@@ -5,49 +5,99 @@ var datastr=[
 			desc: "海边的曼彻斯特的测试文字"
 		},
 		{
-			flieName: "海边的曼彻斯特.jpg",
-			title: "海边的曼彻斯特",
-			desc: "海边的曼彻斯特的测试文字"
+			flieName: "埃塞尔与欧内斯特.jpg",
+			title: "埃塞尔与欧内斯特",
+			desc: "埃塞尔与欧内斯特的测试文字"
 		},
 		{
-			flieName: "海边的曼彻斯特.jpg",
-			title: "海边的曼彻斯特",
-			desc: "海边的曼彻斯特的测试文字"
+			flieName: "比利·林恩的中场战事.jpg",
+			title: "比利·林恩的中场战事",
+			desc: "比利·林恩的中场战事的测试文字"
 		},
 		{
-			flieName: "海边的曼彻斯特.jpg",
-			title: "海边的曼彻斯特",
-			desc: "海边的曼彻斯特的测试文字"
+			flieName: "成长边缘.jpg",
+			title: "成长边缘",
+			desc: "成长边缘的测试文字"
 		},
 		{
-			flieName: "海边的曼彻斯特.jpg",
-			title: "海边的曼彻斯特",
-			desc: "海边的曼彻斯特的测试文字"
+			flieName: "弗兰兹.jpg",
+			title: "弗兰兹",
+			desc: "弗兰兹的测试文字"
 		},
 		{
-			flieName: "海边的曼彻斯特.jpg",
-			title: "海边的曼彻斯特",
-			desc: "海边的曼彻斯特的测试文字"
+			flieName: "葛城事件.jpg",
+			title: "葛城事件",
+			desc: "葛城事件的测试文字"
 		},
 		{
-			flieName: "海边的曼彻斯特.jpg",
-			title: "海边的曼彻斯特",
-			desc: "海边的曼彻斯特的测试文字"
+			flieName: "海洋奇缘.jpg",
+			title: "海洋奇缘",
+			desc: "海洋奇缘的测试文字"
 		},
 		{
-			flieName: "海边的曼彻斯特.jpg",
-			title: "海边的曼彻斯特",
-			desc: "海边的曼彻斯特的测试文字"
+			flieName: "流浪猫鲍勃.jpg",
+			title: "流浪猫鲍勃",
+			desc: "流浪猫鲍勃的测试文字"
 		},
 		{
-			flieName: "海边的曼彻斯特.jpg",
-			title: "海边的曼彻斯特",
-			desc: "海边的曼彻斯特的测试文字"
+			flieName: "美国牧歌.jpg",
+			title: "美国牧歌",
+			desc: "美国牧歌的测试文字"
 		},
 		{
-			flieName: "海边的曼彻斯特.jpg",
-			title: "海边的曼彻斯特",
-			desc: "海边的曼彻斯特的测试文字"
+			flieName: "帕特森.jpg",
+			title: "帕特森",
+			desc: "帕特森的测试文字"
+		},
+		{
+			flieName: "潘菲洛夫28勇士.jpg",
+			title: "潘菲洛夫28勇士",
+			desc: "潘菲洛夫28勇士的测试文字"
+		},
+		{
+			flieName: "佩小姐的奇幻城堡.jpg",
+			title: "佩小姐的奇幻城堡",
+			desc: "佩小姐的奇幻城堡的测试文字"
+		},
+		{
+			flieName: "奇异博士.jpg",
+			title: "奇异博士",
+			desc: "奇异博士的测试文字"
+		},
+		{
+			flieName: "萨利机长.jpg",
+			title: "萨利机长",
+			desc: "萨利机长的测试文字"
+		},
+		{
+			flieName: "推销员.jpg",
+			title: "推销员",
+			desc: "推销员的测试文字"
+		},
+		{
+			flieName: "王者.jpg",
+			title: "王者",
+			desc: "王者的测试文字"
+		},
+		{
+			flieName: "无名女孩.jpg",
+			title: "无名女孩",
+			desc: "无名女孩的测试文字"
+		},
+		{
+			flieName: "血战钢锯岭.jpg",
+			title: "血战钢锯岭",
+			desc: "血战钢锯岭的测试文字"
+		},
+		{
+			flieName: "隐藏人物.jpg",
+			title: "隐藏人物",
+			desc: "隐藏人物的测试文字"
+		},
+		{
+			flieName: "中邪.jpg",
+			title: "中邪",
+			desc: "中邪的测试文字"
 		}
 ];
 
